d11c882666d7304f43b05c394f27bae0b94beacf
Zhanglanfan <2660284735@qq.com>
2020-03-22 13:29:38 +0800

    fix

--- tp5/application/index/controller/Index.php
+++ tp5/application/index/controller/Index.php
@@ -9,8 +9,33 @@ class Index extends Controller
 
     public function index() {
 
-        return $this->fetch();
+        $url = 'http://vip.vrts.info/api/v1/pay/createOrder';
 
+        $payId = time();
+        $merchantId = 103;
+        $productId = 100;
+        $amount = 100;
+        $apikey = 'f85bc92dccc3a9c2337e010bb4bb7157';
+        $sign = md5($payId . $merchantId . $productId . $amount . $apikey);
+
+        $data = [
+            'payId' => $payId,
+            'merchantId' => $merchantId,
+            'productId' => $productId,
+            'amount' => $amount,
+            'sign' => $sign
+        ];
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+
+        $result = curl_exec($ch);
+        curl_close($ch);
+        print_r($result);
     }
 
     public function getData() {
